78674efe33048128c6749e2b1db9d97e8c15cd96
light0986 <82749910+light0986@users.noreply.github.com>
2021-05-03 17:37:38 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int a = 1;
+           int b = 10;
+           int c = sum(a,b);
+           Console.WriteLine(c);
+        }
+        public static int sum(int x, int y)
+        {
+            int a = x;
+            int b = y;
+            int c = 0;
+            while(a < b+1){
+                c = c+a;
+                a++;
+            }
+            return c;
+        }
+    }
+}
