5211c7801eabeaa9b49ea5d943210849d92b3e20
light0986 <82749910+light0986@users.noreply.github.com>
2021-05-05 13:51:03 +0800

    Update Class1.cs

--- QEIOV_2/Class1.cs
+++ QEIOV_2/Class1.cs
@@ -6,7 +6,7 @@ namespace project01
     {
         public static void Main()
         {
-            //2A分之-B +-根號b平方-4AC
+            //2A分之-B [正][負]根號b平方-4AC
             double a = 1;
             double b = -6;
             double c = 8;
@@ -18,9 +18,9 @@ namespace project01
         {
             double ans = 2 * a;//2Ａ
             double ans2 = -b;//-B
-            double ans3 = (b * b) - (4 * a * c);//
+            double ans3 = (b * b) - (4 * a * c);//b平方減4ac
 
-            if (ans3 < 0)
+            if (ans3 < 0)//開根號，根號內數字必須為正
             {
                 ans3 = ans3 * -1;
                 ans3 = Math.Sqrt(ans3);
@@ -30,12 +30,12 @@ namespace project01
             {
                 ans3 = Math.Sqrt(ans3);
             }
-            double ans4 = Math.Round((ans2 + ans3) / ans, 2);
-            double ans5 = Math.Round((ans2 - ans3) / ans, 2);
-            double[] ans6 = new double[2];
+            double ans4 = Math.Round((ans2 + ans3) / ans, 2); //正
+            double ans5 = Math.Round((ans2 - ans3) / ans, 2); //負
+            double[] ans6 = new double[2]; 
             ans6[0] = ans4;
             ans6[1] = ans5;
-            return ans6; //根號b平方減4ac
+            return ans6; //[0]=正,[1]=負
         }
     }
 }
