0b3bb533af67c29a24b0d5c3bff49e0630d10e61
light0986 <82749910+light0986@users.noreply.github.com>
2021-05-06 21:26:04 +0800

    Add files via upload

---
+++ C#_readtxt_macOS/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace prectice01
+{
+    public class Class1
+    {
+        public string cube()
+        {
+            var path = "/Users/light0986/Documents/c#/prectice01/test.txt";
+            StreamReader reader = new StreamReader(path);
+            var input = reader.ReadToEnd();
+            return input;
+        }
+    }
+}
---
+++ C#_readtxt_macOS/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace console01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Result = new prectice01.Class1().cube();
+            Console.WriteLine(Result);
+        }
+
+    }
+}
