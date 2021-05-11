aae7643a60ce5823eec8d4d2527d3cf5d186634a
light0986 <82749910+light0986@users.noreply.github.com>
2021-05-11 14:02:09 +0800

    Add files via upload

---
+++ MVC_SQLite/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using MVC01.Models;
+
+namespace MVC01.Models
+{
+        public class DatabaseContext : DbContext
+        {
+            public DatabaseContext()
+            {
+            }
+
+            public DatabaseContext(DbContextOptions<DatabaseContext> options)
+                : base(options)
+            {
+            }
+
+            public virtual DbSet<Product> Product { get; set; }
+
+        }
+}
---
+++ MVC_SQLite/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MVC01.Models
+{
+    [Table("PRODUCT")]
+    public class Product
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string NAME { get; set; }
+
+        public int PRICE { get; set; }
+
+        public int QUANTITY { get; set; }
+    }
+}
