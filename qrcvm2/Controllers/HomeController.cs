88b25e69d0df3059f08a4863bbd546f7a4dce3db
light0986 <82749910+light0986@users.noreply.github.com>
2022-01-05 17:53:40 +0800

    Add files via upload

---
+++ qrcvm2/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using qrcvm2_web.Models;
+using System.Diagnostics;
+
+namespace qrcvm2_web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult check()
+        {
+            TempData["text01"] = "Hello World!";
+            return RedirectToAction("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
